b8ed58a95fd2c482ae1fb0cb85c2577b4e37d240
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-09-20 23:40:24 -0300

    Aula 05 - Comandos em redis part2

--- redis_raw.py
+++ redis_raw.py
@@ -11,5 +11,25 @@ meu_valor = redis_conn.get("chave_1").decode("utf-8")
 # delete de dados
 redis_conn.delete("chave_1")
 
-print(meu_valor)
-print(type(meu_valor))
+# comandos para hash
+redis_conn.hset("meu_hash", "nome", "joao")
+
+meu_hash = {  # chave de exemploe
+    "nome": "joao",  # field: values
+    "idade": 30,
+    "cidade": "sao paulo",
+}
+redis_conn.hset("meu_hash", "nome", "joao")
+redis_conn.hset("meu_hash", "idade", "30")
+redis_conn.hset("meu_hash", "cidade", "curitiba")
+
+valor_1 = redis_conn.hget("meu_hash", "nome").decode("utf-8")
+
+redis_conn.hdel("meu_hash", "cidade")
+
+# buscas por existência
+elem = redis_conn.exists("chave_1")
+print(elem)
+
+elem2 = redis_conn.hexists("meu_hash", "nome")
+print(elem2)
